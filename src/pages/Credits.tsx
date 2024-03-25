f7d2154702e1a7ad13aabbd0f6a6578b59437696
ThermicWaffle <thermicwaffle+github@gmail.com>
2024-03-24 20:41:30 -0700

    Implement Credits and Version Compatibility partially

--- src/pages/Credits.tsx
+++ src/pages/Credits.tsx
@@ -1,5 +1,14 @@
-const Credits = () => {
-  return <div>Welcome to Credits</div>;
-};
+function Credits() {
+  return (
+    <>
+      <div>Welcome to Credits</div>
+      <iframe
+        src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSV9jpOPAEtFTpAEFAZY1DxLMLQnYDiqkdHinviYVFD5oN4YXA97lY_jinhk23vRzD0lAwkD5-2TsBn/pubhtml?widget=true&amp;headers=false"
+        width="100%"
+        height="100%"
+      />
+    </>
+  );
+}
 
 export default Credits;
--- src/pages/VersionCompatibility.tsx
+++ src/pages/VersionCompatibility.tsx
@@ -1,5 +1,14 @@
-const VersionCompatibility = () => {
-  return <div>Welcome to Version Compatibility</div>;
-};
+function VersionCompatibility() {
+  return (
+    <>
+      <div>Welcome to Version Compatibility</div>
+      <iframe
+        src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQFa5bgQ9zZX2yirR626Up5H0GwGoKPXuil6uyjOO5pGT2NuVURJHcild0ZBR8lC4Pyl3vA_ev_iEVt/pubhtml?widget=true&amp;headers=false"
+        width="100%"
+        height="100%"
+      />
+    </>
+  );
+}
 
 export default VersionCompatibility;
