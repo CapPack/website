d6d391a06d1142de3b57700b64e11a5a48a10cf9
ThermicWaffle <thermicwaffle+github@gmail.com>
2024-03-26 01:00:14 -0700

    Work on Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,17 @@
 const Home = () => {
   return (
     <div className="text-center">
-      <p className="m-10">CapPack</p>
-      <p className="m-10">The Official CaptainSparklez Texture Pack</p>
+      <p className="m-10 font-bold text-6xl">CapPack</p>
+      <p className="m-10 font-semibold text-2xl">
+        The Official CaptainSparklez Texture Pack
+      </p>
       <div className="m-10">
-        <p>Supported Minecraft Versions:</p>
+        <p className="font-bold">Supported Minecraft Versions:</p>
         <p>Java: 1.6.1 - 1.20.4</p>
         <p>Bedrock: All Versions</p>
       </div>
       <div className="m-10">
-        <p>To Enable Custom Item Names:</p>
+        <p className="font-bold">To Enable Custom Item Names:</p>
         <p>
           Java: Open “Options”, then “Language”, and change the language to
           “CaptainSparklez (US)”
