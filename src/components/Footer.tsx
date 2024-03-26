cfe98a420e600c0ab14e096ca49296bc18c999b0
ThermicWaffle <thermicwaffle+github@gmail.com>
2024-03-25 22:38:53 -0700

    Work on styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,20 +1,22 @@
 const Footer = () => {
   return (
-    <div className="text-4xl w-screen text-center flex-auto">
-      <p className="">
-        Made by{" "}
-        <span className="text-thermic-orange decoration-black underline hover:no-underline">
-          <a href="https://thermic.icu" target="_blank">
-            ThermicWaffle
-          </a>
-        </span>{" "}
-        &{" "}
-        <span className="text-greenish underline hover:no-underline decoration-black">
-          <a href="https://linktr.ee/muddyCookie" target="_blank">
-            muddyCookie
-          </a>
-        </span>
-      </p>
+    <div>
+      <div className="text-4xl w-full text-center flex-auto">
+        <p className="">
+          Made by{" "}
+          <span className="text-thermic-orange decoration-black underline hover:no-underline">
+            <a href="https://thermic.icu" target="_blank">
+              ThermicWaffle
+            </a>
+          </span>{" "}
+          &{" "}
+          <span className="text-greenish underline hover:no-underline decoration-black">
+            <a href="https://linktr.ee/muddyCookie" target="_blank">
+              muddyCookie
+            </a>
+          </span>
+        </p>
+      </div>
     </div>
   );
 };
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,8 +4,12 @@ import CapPackImage from "../assets/CapPack.png";
 const Navbar = () => {
   const location = useLocation();
 
+  const isActive = (path: string) => {
+    return location.pathname === path ? "font-bold" : "hover:font-bold";
+  };
+
   return (
-    <div className="flex flex-row w-full">
+    <div className="flex flex-row w-full items-center">
       <img src={CapPackImage} alt="CapPack Logo" className="w-32 mx-4 my-4" />
       <p className="w-full m-auto items-center mx-4 text-2xl">
         <span className="font-bold hover:underline">
@@ -19,49 +23,43 @@ const Navbar = () => {
         </span>
         {"  "}Official CaptainSparklez Texture Pack Website
       </p>
-      <div className="flex flex-row flex-wrap justify-end w-full mx-6">
-        <Link
-          to="/"
-          className={`mx-4 text-center m-auto text-xl ${
-            location.pathname === "/" ? "font-bold" : "hover:font-bold"
-          }`}
-        >
+      <div className="flex flex-row flex-wrap justify-end w-full mx-4 items-center">
+        <Link to="/" className={`m-4 text-xl ${isActive("/")}`}>
           Home
         </Link>
         <Link
           to="/downloads"
-          className={`mx-4 text-center m-auto text-xl ${
-            location.pathname === "/downloads" ? "font-bold" : "hover:font-bold"
-          }`}
+          className={`m-4 text-xl ${isActive("/downloads")}`}
         >
           Downloads
         </Link>
         <Link
           to="/version-compatibility"
-          className={`mx-4 text-center m-auto text-xl ${
-            location.pathname === "/version-compatibility"
-              ? "font-bold"
-              : "hover:font-bold"
-          }`}
+          className={`m-4 text-xl ${isActive("/version-compatibility")}`}
         >
           Version Compatibility
         </Link>
-        <Link
-          to="/credits"
-          className={`mx-4 text-center m-auto text-xl ${
-            location.pathname === "/credits" ? "font-bold" : "hover:font-bold"
-          }`}
-        >
+        <Link to="/credits" className={`m-4 text-xl ${isActive("/credits")}`}>
           Credits
         </Link>
-        <a href="https://discord.gg/aDE8TYA" target="_blank" rel="noopener noreferrer">
+        <a
+          href="https://discord.gg/aDE8TYA"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="link-icon"
+        >
           <img
             src="https://assets-global.website-files.com/6257adef93867e50d84d30e2/636e0a69f118df70ad7828d4_icon_clyde_blurple_RGB.svg"
             className="w-10 mx-4 my-4 hover:w-11"
             alt="discord logo"
           />
         </a>
-        <a href="https://github.com/CapPack/the-captainsparklez-texture-pack" target="_blank" rel="noopener noreferrer">
+        <a
+          href="https://github.com/CapPack/the-captainsparklez-texture-pack"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="link-icon"
+        >
           <img
             src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
             className="w-10 mx-4 my-4 hover:w-11"
