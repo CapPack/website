f8fc2266affb73b9893f1bfe4ace9f08ef194068
ThermicWaffle <50603210+ThermicWaffle@users.noreply.github.com>
2024-03-29 15:11:34 -0700

    Update Layout.tsx

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import { useLocation } from "react-router-dom";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
+  const location = useLocation();
+  const isEmbedPage = location.pathname === "/version-compatibility" || location.pathname === "/credits";
+
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
-      <div className="flex-grow">{children}</div>
+      <div className={`${isEmbedPage ? "h-full" : "flex-grow"}`}>{children}</div>
       <Footer />
     </div>
   );
