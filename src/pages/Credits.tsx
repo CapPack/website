18147434ad97696678cd98587da89d0b164e2415
ThermicWaffle <50603210+ThermicWaffle@users.noreply.github.com>
2024-03-29 15:29:53 -0700

    Update Credits.tsx

--- src/pages/Credits.tsx
+++ src/pages/Credits.tsx
@@ -1,5 +1,4 @@
-import React, { useEffect } from "react";
-import Layout from "./Layout";
+import { useEffect } from "react";
 
 function Credits() {
   useEffect(() => {
@@ -7,12 +6,12 @@ function Credits() {
   }, []);
 
   return (
-    <Layout fullHeight>
+    <>
       <iframe
         src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSV9jpOPAEtFTpAEFAZY1DxLMLQnYDiqkdHinviYVFD5oN4YXA97lY_jinhk23vRzD0lAwkD5-2TsBn/pubhtml?widget=true&amp;headers=false"
         className="px-10 w-full h-full"
       />
-    </Layout>
+    </>
   );
 }
 
