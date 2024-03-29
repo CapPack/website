99d256621ce131e3233867509d1b84aeba17c2fc
ThermicWaffle <50603210+ThermicWaffle@users.noreply.github.com>
2024-03-29 15:27:59 -0700

    Update Layout.tsx

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -2,15 +2,11 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
-interface LayoutProps {
-  fullHeight?: boolean;
-}
-
-const Layout: React.FC<LayoutProps> = ({ children, fullHeight = false }) => {
+const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className={`flex flex-col min-h-screen ${fullHeight ? 'h-full' : 'flex-grow'}`}>
+    <div className="flex flex-col min-h-screen">
       <Navbar />
-      <div>{children}</div>
+      <div className="flex-grow">{children}</div>
       <Footer />
     </div>
   );
