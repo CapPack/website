3893f2a3ad4dc60e0500286347f8582013205b28
ThermicWaffle <50603210+ThermicWaffle@users.noreply.github.com>
2024-03-29 15:58:48 -0700

    Update VersionCompatibility.tsx

--- src/pages/VersionCompatibility.tsx
+++ src/pages/VersionCompatibility.tsx
@@ -1,5 +1,4 @@
 import { useEffect } from "react";
-import Layout from "../layouts/Layout";
 
 function VersionCompatibility() {
   useEffect(() => {
@@ -7,12 +6,12 @@ function VersionCompatibility() {
   }, []);
 
   return (
-    <Layout fullHeight>
+    <>
       <iframe
         src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQFa5bgQ9zZX2yirR626Up5H0GwGoKPXuil6uyjOO5pGT2NuVURJHcild0ZBR8lC4Pyl3vA_ev_iEVt/pubhtml?widget=true&amp;headers=false"
         className="px-10 w-full h-full"
       />
-    </Layout>
+    </>
   );
 }
 
