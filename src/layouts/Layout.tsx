76bc07b11ed8eccabba2ca93b0accf764b461fd2
ThermicWaffle <50603210+ThermicWaffle@users.noreply.github.com>
2024-03-29 15:16:59 -0700

    Update Layout.tsx

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,7 +1,7 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import { useLocation } from "react-router-dom";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   const location = useLocation();
@@ -10,7 +10,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
-      <div className={`${isEmbedPage ? "h-full" : "flex-grow"}`}>{children}</div>
+      <div className={`flex-grow ${isEmbedPage ? "h-full" : ""}`}>{children}</div>
       <Footer />
     </div>
   );
