bb251ebc0952458e71d430de482bea02f9f245ae
ThermicWaffle <50603210+ThermicWaffle@users.noreply.github.com>
2024-03-29 17:26:42 -0700

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="text-4xl text-center items-center m-8">
+    <div className="text-4xl text-center items-center my-8 pb-8">
       <p>
         Made by{" "}
         <a
