ca869e9e7f9bb72e7066b82fb523458c0788162d
ThermicWaffle <50603210+ThermicWaffle@users.noreply.github.com>
2024-03-29 17:21:49 -0700

    Update Layout.tsx

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -6,7 +6,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex flex-col h-screen">
       <Navbar />
-      <div className="h-full">{children}</div>
+      <div className="flex-grow">{children}</div>
       <Footer />
     </div>
   );
