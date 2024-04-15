b7ff00470bf5fcf34e9dce7680098a6d894f88a5
ThermicWaffle <50603210+ThermicWaffle@users.noreply.github.com>
2024-04-15 14:26:45 -0700

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import CapPackImage from "../assets/CapPack.png";
 
